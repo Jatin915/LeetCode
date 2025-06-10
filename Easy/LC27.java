ad03910fb58198847f2e0e365407ba2f1421dc9e
gJatin <gjatin278@gmail.com>
2025-06-11 01:15:04 +0530

    Added Solution

---
+++ Easy/LC27.java
@@ -0,0 +1,27 @@
+// LeetCode 27: Remove Element
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count=0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == val){
+                count++;
+            }
+        }
+        int index=nums.length-1;
+        for(int i=nums.length-1; i>((nums.length-1)-count); i--) {
+            for(int j=index; j>=0; j--) {
+                if(nums[j] == val){
+                    nums[j]=nums[i];
+                    nums[i]=val;
+                    index=index-1;
+                    break;
+                }
+            }
+        }
+        return nums.length-count;
+    }
+}
