cc8d33cb4512982fedfd0c9bd68480b5b1892238
gJatin <gjatin278@gmail.com>
2025-06-18 20:42:00 +0530

    Added Solution

---
+++ Easy/LC1672.java
@@ -0,0 +1,18 @@
+// LeetCode 1672: Richest Customer Wealth
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int sum=0, store=0;
+        for(int i=0; i<accounts.length; i++){
+            for(int j=0; j<accounts[i].length; j++){
+                store += accounts[i][j];
+            }
+            if(store > sum)
+                sum=store;
+                store=0;
+        }
+        return sum;   
+    }
+}
