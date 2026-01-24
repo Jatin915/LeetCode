6335872d57ca23dc5a4ff5593ab71b0835c5b90a
gJatin <gjatin278@gmail.com>
2026-01-24 23:29:45 +0530

    LC11: add solution

---
+++ Medium/LC11.java
@@ -0,0 +1,27 @@
+// LeetCode 11: Container With Most Water
+// Difficulty: Medium
+// Link: https://leetcode.com/problems/container-with-most-water/description/
+
+public class LC11 {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentHeight = Math.min(height[left], height[right]);
+            int currentWidth = right - left;
+            int currentArea = currentHeight * currentWidth;
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+    
+}
