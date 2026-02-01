5d73f43c67c64aed0d620969023df159212257ae
gJatin <gjatin278@gmail.com>
2026-02-01 23:57:40 +0530

    LC27: optimize solution

--- Easy/LC27.java
+++ Easy/LC27.java
@@ -4,23 +4,14 @@
 
 class LC27 {
     public int removeElement(int[] nums, int val) {
-        int count=0;
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] == val){
-                count++;
+        int i = 0; // slow pointer for valid elements
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
             }
         }
-        int index=nums.length-1;
-        for(int i=nums.length-1; i>((nums.length-1)-count); i--) {
-            for(int j=index; j>=0; j--) {
-                if(nums[j] == val){
-                    nums[j]=nums[i];
-                    nums[i]=val;
-                    index=index-1;
-                    break;
-                }
-            }
-        }
-        return nums.length-count;
+        return i;
     }
 }
