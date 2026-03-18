dec1a751fefb916f4a0fff8ea8dc0429d068b43a
gJatin <gjatin278@gmail.com>
2026-03-18 23:15:23 +0530

    LC206: add solution

---
+++ Easy/LC206.java
@@ -0,0 +1,28 @@
+// LeetCode 206: Reverse Linked List
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/reverse-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class LC206 {
+    public ListNode reverseList(ListNode head) {
+       ListNode prev = null;
+       ListNode curr = head;
+
+        while(curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
