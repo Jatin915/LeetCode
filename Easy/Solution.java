1067c94e18a7ac3956f1549b00a9044f3a98cea3
gJatin <gjatin278@gmail.com>
2026-03-21 22:21:22 +0530

    LC141: add solution

---
+++ Easy/Solution.java
@@ -0,0 +1,32 @@
+// LeetCode 141: Linked List Cycle
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/linked-list-cycle/description/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        if(head != null && head.next != null) {
+            do {
+                fast = fast.next.next;
+                slow = slow.next;
+                if(fast == slow) {
+                    return true;
+                }
+            } while(slow != null && fast != null && fast.next != null);
+        }
+        return false;
+    }
+}
