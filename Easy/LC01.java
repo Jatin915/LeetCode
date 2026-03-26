45819e68eeeb7cd09bbf4a2f3c188894378548d9
gJatin <gjatin278@gmail.com>
2026-03-26 23:38:36 +0530

    LC01: add solution

---
+++ Easy/LC01.java
@@ -0,0 +1,18 @@
+// LeetCode 01: Two Sum
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/two-sum/
+
+import java.util.HashMap;
+class LC01 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if(map.containsKey(complement)) 
+                return new int[]{map.get(complement),i};
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
